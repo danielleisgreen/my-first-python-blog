27e3afcd92d671bb275fbb661c23378447119504
DtheG <danielle.green@gmail.com>
2020-05-29 22:10:16 +0100

    Update settings.py
    Added test line

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,6 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+#This is a change I'm adding as a test
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
